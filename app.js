ca267ef6fd6bfe209622f6afd890e0319f81513a
job-mean <fahad@it22dev.nl>
2019-01-01 00:52:38 +0500

    testing

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 const fs = require('fs');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/register');
